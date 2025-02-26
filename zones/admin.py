15cc0148f47a384d5cb2a7e1f7856adce7b5fa23
netwizd <63255739+netwizd@users.noreply.github.com>
2025-02-26 14:29:35 +0300

    nldns
    Added models for zones and records

--- zones/admin.py
+++ zones/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Zone, Record
 
-# Register your models here.
+admin.site.register(Zone)
+admin.site.register(Record)
--- zones/models.py
+++ zones/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Zone(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    type = models.CharField(max_length=10, choices=[
+        ('forward', 'Forward'),
+        ('reverse', 'Reverse')
+    ])
+    ttl = models.IntegerField(default=3600)
+
+    def __str__(self):
+        return self.name
+
+
+class Record(models.Model):
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE, related_name='records')
+    type = models.CharField(max_length=10, choices=[
+        ('A', 'A'),
+        ('AAAA', 'AAAA'),
+        ('CNAME', 'CNAME'),
+        ('MX', 'MX'),
+        ('TXT', 'TXT'),
+        ('NS', 'NS'),
+        ('PTR', 'PTR'),
+        ('SRV', 'SRV'),
+        ('CAA', 'CAA'),
+        ('SOA', 'SOA'),
+    ])
+    name = models.CharField(max_length=255, blank=True, null=True)
+    value = models.CharField(max_length=255)
+    priority = models.IntegerField(blank=True, null=True)  # Для MX и SRV
+    ttl = models.IntegerField(default=3600)
+
+    def __str__(self):
+        return f"{self.name} {self.type} {self.value}"
