5eab8369a7f592c13314de2e45b616e880adca06
netwizd <63255739+netwizd@users.noreply.github.com>
2025-02-26 14:03:54 +0300

    nldns
    Added initial Django apps and routing

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zones',
+    'cache',
+    'config',
+    'notifications',
+    'users',
 ]
 
 MIDDLEWARE = [
